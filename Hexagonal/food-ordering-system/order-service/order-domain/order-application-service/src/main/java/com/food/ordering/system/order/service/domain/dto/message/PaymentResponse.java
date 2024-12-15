c2702a517449aaef69d9e0299d7820fda31c01d6
Engineer-kim <ddang0103@naver.com>
2024-12-15 13:12:32 +0900

    Feature: 주문쪽 Message 퍼블리셔 인터페이스 작성 ,  클라이언트측에서의 요청받아 DB와의 트랜잭션 레이어 작성

--- Hexagonal/food-ordering-system/order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/dto/message/PaymentResponse.java
+++ Hexagonal/food-ordering-system/order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/dto/message/PaymentResponse.java
@@ -11,7 +11,7 @@ import java.util.List;
 @Getter
 @Builder
 @AllArgsConstructor
-public class PaymentModel {
+public class PaymentResponse {
     private String id;
     private String sagaId;
     private String orderId;
---
+++ Hexagonal/food-ordering-system/order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/ports/input/service/OrderApplicationService.java
@@ -0,0 +1,16 @@
+package com.food.ordering.system.order.service.domain.ports.input.service;
+
+import com.food.ordering.system.order.service.domain.dto.create.CreateOrderCommand;
+import com.food.ordering.system.order.service.domain.dto.create.CreateOrderResponse;
+import com.food.ordering.system.order.service.domain.dto.track.TrackOrderQuery;
+import com.food.ordering.system.order.service.domain.dto.track.TrackOrderResponse;
+import jakarta.validation.Valid;
+
+
+//클라이언트가 사용
+public interface OrderApplicationService {
+
+   CreateOrderResponse createOrder(@Valid  CreateOrderCommand  createOrderCommand);
+
+  TrackOrderResponse trackOrder(@Valid TrackOrderQuery trackOrderQuery);
+}
