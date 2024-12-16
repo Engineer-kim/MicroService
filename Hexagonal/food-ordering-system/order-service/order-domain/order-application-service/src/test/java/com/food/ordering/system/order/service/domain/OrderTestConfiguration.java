7184111317b2789a6df2cff47affd0490373b190
Engineer-kim <ddang0103@naver.com>
2024-12-16 12:36:30 +0900

    Feature: 카프카 & 주키퍼 설정 파일 작성및 실습

--- Hexagonal/food-ordering-system/order-service/order-domain/order-application-service/src/test/java/com/food/ordering/system/order/service/domain/OrderTestConfiguration.java
+++ Hexagonal/food-ordering-system/order-service/order-domain/order-application-service/src/test/java/com/food/ordering/system/order/service/domain/OrderTestConfiguration.java
@@ -1,7 +1,31 @@
 package com.food.ordering.system.order.service.domain;
 
+import com.food.ordering.system.order.service.domain.ports.outport.repository.CustomerRepository;
+import com.food.ordering.system.order.service.domain.ports.outport.repository.OrderRepository;
+import com.food.ordering.system.order.service.domain.ports.outport.repository.RestaurantRepository;
+import org.mockito.Mockito;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication(scanBasePackages = "com.food.ordering.system")
 public class OrderTestConfiguration {
+    @Bean
+    public OrderRepository orderRepository() {
+        return Mockito.mock(OrderRepository.class);
+    }
+
+    @Bean
+    public CustomerRepository customerRepository() {
+        return Mockito.mock(CustomerRepository.class);
+    }
+
+    @Bean
+    public RestaurantRepository restaurantRepository() {
+        return Mockito.mock(RestaurantRepository.class);
+    }
+    @Bean
+    public OrderDomainService orderDomainService() {
+        return new OrderDomainServiceImpl();
+    }
+
 }
