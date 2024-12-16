dccdfacfc4b551151f38f564eddbd8d330523ad1
Engineer-kim <ddang0103@naver.com>
2024-12-16 15:18:54 +0900

    Feature: 카프카 생산자 일반 모듈 구현

---
+++ Hexagonal/food-ordering-system/infrastructure/kafka/kafka-producer/src/main/java/com/food/ordering/system/kafka/producer/KafkaProducerConfig.java
@@ -0,0 +1,60 @@
+package com.food.ordering.system.kafka.producer;
+
+import com.food.ordering.system.kafka.config.data.KafkaConfigData;
+import com.food.ordering.system.kafka.config.data.KafkaProducerConfigData;
+import org.apache.avro.specific.SpecificRecordBase;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.checkerframework.checker.units.qual.K;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaProducerConfig <K extends Serializable, V extends SpecificRecordBase>{
+    //Serializable 는 마커 인터페이스로 아무런 함수가 없는 인터페이스지만
+    //직렬화를 가능하다는것을 암묵적으로 명시함 런타임시 직렬화 관련 기능 제공
+    private final KafkaConfigData kafkaConfigData;
+    private final KafkaProducerConfigData kafkaProducerConfigData;
+
+
+    public KafkaProducerConfig(KafkaConfigData kafkaConfigData, KafkaProducerConfigData kafkaProducerConfigData) {
+        this.kafkaConfigData = kafkaConfigData;
+        this.kafkaProducerConfigData = kafkaProducerConfigData;
+    }
+
+
+    @Bean
+    public Map<String, Object> producerConfig() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaConfigData.getBootstrapServers());
+        props.put(kafkaConfigData.getSchemaRegistryUrlKey(), kafkaConfigData.getSchemaRegistryUrl());
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, kafkaProducerConfigData.getKeySerializerClass());
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, kafkaProducerConfigData.getValueSerializerClass());
+        props.put(ProducerConfig.BATCH_SIZE_CONFIG, kafkaProducerConfigData.getBatchSize() *
+                kafkaProducerConfigData.getBatchSizeBoostFactor());
+        props.put(ProducerConfig.LINGER_MS_CONFIG, kafkaProducerConfigData.getLingerMs());
+        props.put(ProducerConfig.COMPRESSION_TYPE_CONFIG, kafkaProducerConfigData.getCompressionType());
+        props.put(ProducerConfig.ACKS_CONFIG, kafkaProducerConfigData.getAcks());
+        props.put(ProducerConfig.REQUEST_TIMEOUT_MS_CONFIG, kafkaProducerConfigData.getRequestTimeoutMs());
+        props.put(ProducerConfig.RETRIES_CONFIG, kafkaProducerConfigData.getRetryCount());
+        return props;
+    }
+    
+    //Kafka 프로듀서 생성
+    @Bean
+    public ProducerFactory<K, V> producerFactory() {
+        return new DefaultKafkaProducerFactory<>(producerConfig());
+    }
+
+    //Kafka에 메시지를 전송
+    @Bean
+    public KafkaTemplate<K, V> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
+}
---
+++ Hexagonal/food-ordering-system/infrastructure/kafka/kafka-producer/src/main/java/com/food/ordering/system/kafka/producer/service/impl/KafkaProducerImpl.java
@@ -0,0 +1,50 @@
+package com.food.ordering.system.kafka.producer.service.impl;
+
+import com.food.ordering.system.kafka.producer.exception.KafkaProducerException;
+import com.food.ordering.system.kafka.producer.service.KafkaProducer;
+import jakarta.annotation.PreDestroy;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.avro.specific.SpecificRecordBase;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.stereotype.Component;
+
+import java.io.Serializable;
+import java.util.concurrent.CompletableFuture;
+
+@Slf4j
+@Component
+public class KafkaProducerImpl<K extends Serializable, V extends SpecificRecordBase> implements KafkaProducer<K, V> {
+
+   private final KafkaTemplate<K, V> kafkaTemplate;
+
+    public KafkaProducerImpl(KafkaTemplate<K, V> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+    @Override
+    public CompletableFuture<SendResult<K, V>> send(String topicName, K key, V message) {
+        log.info("Sending message={} to topic={}", message, topicName);
+        try {
+            return CompletableFuture.supplyAsync(() -> {
+                try {
+                    return kafkaTemplate.send(topicName, key, message).get();
+                } catch (Exception e) {
+                    throw new KafkaProducerException("메시지를 Kafka에 전송하는 데 실패했습니다. 키: " + key + ", 메시지: " + message + ", 오류: " + e.getMessage());
+                }
+            });
+        } catch (Exception ex) {
+            log.error("Error on kafka producer with key: {}, message: {}", key, message, ex);
+            throw new KafkaProducerException("Kafka 메시지를 처리하는 데 실패했습니다. 키: " + key + ", 메시지: " + message + ", 오류: " + ex.getMessage());
+        }
+    }
+
+    @PreDestroy
+    public void close() {
+        if (kafkaTemplate != null) {
+            log.info("Closing kafka producer!");
+            kafkaTemplate.destroy();
+        }
+    }
+
+
+}
