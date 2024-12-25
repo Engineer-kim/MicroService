d7ec8420ca58940a8b2a4d7e758490ba64b2a3cb
Engineer-kim <ddang0103@naver.com>
2024-12-25 18:00:56 +0900

    Add: 헷갈리는 부분에 대한 주석 추가
    이너 빌더 작성(빌더 어노테이션이 아닌 직접 수동으로 작성해봄)

--- Hexagonal/food-ordering-system/restaurant-service/restaurant-domain/restaurant-domain-core/src/main/java/com/food/ordering/system/restaurant/service/domain/entity/Product.java
+++ Hexagonal/food-ordering-system/restaurant-service/restaurant-domain/restaurant-domain-core/src/main/java/com/food/ordering/system/restaurant/service/domain/entity/Product.java
@@ -16,6 +16,7 @@ public class Product extends BaseEntity<ProductId> {
         this.available = available;
     }
 
+    // 빌더 패턴을 사용하여 Product 객체를  실제로 생성하는 생성자  ==> 실제로 프로덕트의 각필드 초기화 부문
     private Product(Builder builder) {
         setId(builder.productId);
         name = builder.name;
@@ -24,6 +25,7 @@ public class Product extends BaseEntity<ProductId> {
         available = builder.available;
     }
 
+    // 빌더 객체를 생성하는 정적 메서드(빌더 패턴을 사용하여 객체를 생성하는 데 도움을 주는 역할)
     public static Builder builder() {
         return new Builder();
     }
@@ -44,6 +46,7 @@ public class Product extends BaseEntity<ProductId> {
         return available;
     }
 
+    // Product 객체를 생성하기 위한 Builder 클래스 (프로덕트 빌더를 생성) ==> build() 메서드를 호출하여 완료 시키는거
     public static final class Builder {
         private ProductId productId;
         private String name;
