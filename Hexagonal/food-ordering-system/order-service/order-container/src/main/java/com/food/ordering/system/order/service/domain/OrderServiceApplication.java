b942ec47a8f4d195b53a7d7a6a9ab29ad6124b18
Engineer-kim <ddang0103@naver.com>
2024-12-26 19:41:17 +0900

    Feature: 레스토랑 도메인쪽의 트랜잭션 로직 작성
    DataAccess Layer 인 Repository 로직 작성
    + 일부 파일 위치 변경및 삭제

--- Hexagonal/food-ordering-system/order-service/order-container/src/main/java/com/food/ordering/system/order/service/domain/OrderServiceApplication.java
+++ Hexagonal/food-ordering-system/order-service/order-container/src/main/java/com/food/ordering/system/order/service/domain/OrderServiceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EnableJpaRepositories(basePackages = { "com.food.ordering.system.order.service.dataaccess", "com.food.ordering.system.data" })
+@EnableJpaRepositories(basePackages = { "com.food.ordering.system.order.service.dataaccess", "com.food.ordering.system.dataaccess" })
 //엔티티 스캔
 @EntityScan(basePackages = { "com.food.ordering.system.order.service.dataaccess", "com.food.ordering.system.dataaccess"})
 //지정된 패키지에서 Spring 컴포넌트를 검색합니다. 여기서는 com.food.ordering.system 패키지를 스캔
