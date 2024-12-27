38d270790b6eefe203c7034f2df7ef7bd01b7ff5
Engineer-kim <ddang0103@naver.com>
2024-12-27 17:47:52 +0900

    Feature: 주문 결제 쪽에 SAGA 패턴 구현
    추후 사용할 로직에 대한 SAGA 패턴에 대한 기본코드 작성

---
+++ Hexagonal/food-ordering-system/common/common-domain/src/main/java/com/food/ordering/system/domain/event/EmptyEvent.java
@@ -0,0 +1,17 @@
+package com.food.ordering.system.domain.event;
+
+
+//트랜잭션 실패시 롤백하기 위한 빈 클래스
+public final class EmptyEvent implements DomainEvent<Void>{
+
+    public  static final EmptyEvent INSTANCE = new EmptyEvent();
+
+    private EmptyEvent(){
+
+    }
+
+    @Override
+    public void fire() {
+
+    }
+}
