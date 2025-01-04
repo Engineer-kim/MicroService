3be1869cce0b60a08205cff52be4ced755d8e5c9
Engineer-kim <ddang0103@naver.com>
2025-01-04 15:54:28 +0900

    Feature: 카프카 메세지 발행하는부분의 아웃박스 패턴 적용
    아웃박스 패턴 적용으로 인한 기존 취소 성공 실패에 해당하는 상태 메세지 퍼블리셔 통합

--- Hexagonal/food-ordering-system/payment-service/payment-messaging/src/main/java/com/food/ordering/system/payment/service/messaging/publisher/kafka/PaymentCompletedKafkaMessagePublisher.java
+++ Hexagonal/food-ordering-system/payment-service/payment-messaging/src/main/java/com/food/ordering/system/payment/service/messaging/publisher/kafka/PaymentCompletedKafkaMessagePublisher.java
@@ -1,54 +1,54 @@
-package com.food.ordering.system.payment.service.messaging.publisher.kafka;
-
-import com.food.ordering.system.domain.config.PaymentServiceConfigData;
-import com.food.ordering.system.domain.ports.output.message.publisher.PaymentCompletedMessagePublisher;
-import com.food.ordering.system.kafka.order.avro.model.PaymentRequestAvroModel;
-import com.food.ordering.system.kafka.order.avro.model.PaymentResponseAvroModel;
-import com.food.ordering.system.kafka.producer.KafkaMessageHelper;
-import com.food.ordering.system.kafka.producer.service.KafkaProducer;
-import com.food.ordering.system.payment.service.domain.event.PaymentCompletedEvent;
-import com.food.ordering.system.payment.service.messaging.mapper.PaymentMessagingDataMapper;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.kafka.support.SendResult;
-import org.springframework.stereotype.Component;
-
-import java.util.concurrent.CompletableFuture;
-
-@Slf4j
-@Component
-public class PaymentCompletedKafkaMessagePublisher implements PaymentCompletedMessagePublisher {
-
-    private final PaymentMessagingDataMapper paymentMessagingDataMapper;
-    private final KafkaProducer<String, PaymentResponseAvroModel> kafkaProducer;
-    private final PaymentServiceConfigData paymentServiceConfigData;
-    private final KafkaMessageHelper kafkaMessageHelper;
-
-    public PaymentCompletedKafkaMessagePublisher(PaymentMessagingDataMapper paymentMessagingDataMapper, KafkaProducer<String, PaymentResponseAvroModel> kafkaProducer, PaymentServiceConfigData paymentServiceConfigData, KafkaMessageHelper kafkaMessageHelper) {
-        this.paymentMessagingDataMapper = paymentMessagingDataMapper;
-        this.kafkaProducer = kafkaProducer;
-        this.paymentServiceConfigData = paymentServiceConfigData;
-        this.kafkaMessageHelper = kafkaMessageHelper;
-    }
-
-
-    @Override
-    public void publish(PaymentCompletedEvent domainEvent) {
-
-        String orderId = domainEvent.getPayment().getOrderId().getValue().toString();
-        log.info("Received PaymentCompletedEvent for order id: {}", orderId );
-        PaymentResponseAvroModel paymentResponseAvroModel = paymentMessagingDataMapper.paymentCompletEventToPaymentResponseAvroModel(domainEvent);
-        // Kafka 메시지 전송
-        CompletableFuture<SendResult<String, PaymentResponseAvroModel>> future =
-                kafkaProducer.send(
-                        paymentServiceConfigData.getPaymentRequestTopicName(),
-                        orderId,
-                        paymentResponseAvroModel
-                );
-
-        // 전송 결과 처리
-        future.whenComplete((result, exception) ->
-                kafkaMessageHelper.getKafkaCallBack(result ,paymentServiceConfigData.getPaymentRequestTopicName() ,exception, orderId, ""));
-
-        log.info("PaymentResponseAvroModel sent to kafka for order id: {} and saga id: {}", paymentResponseAvroModel.getOrderId(), "");
-    }
-}
+//package com.food.ordering.system.payment.service.messaging.publisher.kafka;
+//
+//import com.food.ordering.system.domain.config.PaymentServiceConfigData;
+//import com.food.ordering.system.domain.ports.output.message.publisher.PaymentCompletedMessagePublisher;
+//import com.food.ordering.system.kafka.order.avro.model.PaymentRequestAvroModel;
+//import com.food.ordering.system.kafka.order.avro.model.PaymentResponseAvroModel;
+//import com.food.ordering.system.kafka.producer.KafkaMessageHelper;
+//import com.food.ordering.system.kafka.producer.service.KafkaProducer;
+//import com.food.ordering.system.payment.service.domain.event.PaymentCompletedEvent;
+//import com.food.ordering.system.payment.service.messaging.mapper.PaymentMessagingDataMapper;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.kafka.support.SendResult;
+//import org.springframework.stereotype.Component;
+//
+//import java.util.concurrent.CompletableFuture;
+//
+//@Slf4j
+//@Component
+//public class PaymentCompletedKafkaMessagePublisher implements PaymentCompletedMessagePublisher {
+//
+//    private final PaymentMessagingDataMapper paymentMessagingDataMapper;
+//    private final KafkaProducer<String, PaymentResponseAvroModel> kafkaProducer;
+//    private final PaymentServiceConfigData paymentServiceConfigData;
+//    private final KafkaMessageHelper kafkaMessageHelper;
+//
+//    public PaymentCompletedKafkaMessagePublisher(PaymentMessagingDataMapper paymentMessagingDataMapper, KafkaProducer<String, PaymentResponseAvroModel> kafkaProducer, PaymentServiceConfigData paymentServiceConfigData, KafkaMessageHelper kafkaMessageHelper) {
+//        this.paymentMessagingDataMapper = paymentMessagingDataMapper;
+//        this.kafkaProducer = kafkaProducer;
+//        this.paymentServiceConfigData = paymentServiceConfigData;
+//        this.kafkaMessageHelper = kafkaMessageHelper;
+//    }
+//
+//
+//    @Override
+//    public void publish(PaymentCompletedEvent domainEvent) {
+//
+//        String orderId = domainEvent.getPayment().getOrderId().getValue().toString();
+//        log.info("Received PaymentCompletedEvent for order id: {}", orderId );
+//        PaymentResponseAvroModel paymentResponseAvroModel = paymentMessagingDataMapper.paymentCompletEventToPaymentResponseAvroModel(domainEvent);
+//        // Kafka 메시지 전송
+//        CompletableFuture<SendResult<String, PaymentResponseAvroModel>> future =
+//                kafkaProducer.send(
+//                        paymentServiceConfigData.getPaymentRequestTopicName(),
+//                        orderId,
+//                        paymentResponseAvroModel
+//                );
+//
+//        // 전송 결과 처리
+//        future.whenComplete((result, exception) ->
+//                kafkaMessageHelper.getKafkaCallBack(result ,paymentServiceConfigData.getPaymentRequestTopicName() ,exception, orderId, ""));
+//
+//        log.info("PaymentResponseAvroModel sent to kafka for order id: {} and saga id: {}", paymentResponseAvroModel.getOrderId(), "");
+//    }
+//}
--- Hexagonal/food-ordering-system/payment-service/payment-messaging/src/main/java/com/food/ordering/system/payment/service/messaging/publisher/kafka/PaymentFailedKafkaMessagePublisher.java
+++ Hexagonal/food-ordering-system/payment-service/payment-messaging/src/main/java/com/food/ordering/system/payment/service/messaging/publisher/kafka/PaymentFailedKafkaMessagePublisher.java
@@ -1,56 +1,56 @@
-package com.food.ordering.system.payment.service.messaging.publisher.kafka;
-
-import com.food.ordering.system.domain.config.PaymentServiceConfigData;
-import com.food.ordering.system.domain.ports.output.message.publisher.PaymentCancelledMessagePublisher;
-import com.food.ordering.system.domain.ports.output.message.publisher.PaymentFailedMessagePublisher;
-import com.food.ordering.system.kafka.order.avro.model.PaymentResponseAvroModel;
-import com.food.ordering.system.kafka.producer.KafkaMessageHelper;
-import com.food.ordering.system.kafka.producer.service.KafkaProducer;
-import com.food.ordering.system.payment.service.domain.event.PaymentCancelledEvent;
-import com.food.ordering.system.payment.service.domain.event.PaymentEvent;
-import com.food.ordering.system.payment.service.domain.event.PaymentFailedEvent;
-import com.food.ordering.system.payment.service.messaging.mapper.PaymentMessagingDataMapper;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.kafka.support.SendResult;
-import org.springframework.stereotype.Component;
-
-import java.util.concurrent.CompletableFuture;
-
-@Slf4j
-@Component
-public class PaymentFailedKafkaMessagePublisher implements PaymentFailedMessagePublisher {
-
-    private final PaymentMessagingDataMapper paymentMessagingDataMapper;
-    private final KafkaProducer<String, PaymentResponseAvroModel> kafkaProducer;
-    private final PaymentServiceConfigData paymentServiceConfigData;
-    private final KafkaMessageHelper kafkaMessageHelper;
-
-    public PaymentFailedKafkaMessagePublisher(PaymentMessagingDataMapper paymentMessagingDataMapper, KafkaProducer<String, PaymentResponseAvroModel> kafkaProducer, PaymentServiceConfigData paymentServiceConfigData, KafkaMessageHelper kafkaMessageHelper) {
-        this.paymentMessagingDataMapper = paymentMessagingDataMapper;
-        this.kafkaProducer = kafkaProducer;
-        this.paymentServiceConfigData = paymentServiceConfigData;
-        this.kafkaMessageHelper = kafkaMessageHelper;
-    }
-
-
-    @Override
-    public void publish(PaymentFailedEvent domainEvent) {
-
-        String orderId = domainEvent.getPayment().getOrderId().getValue().toString();
-        log.info("Received PaymentFailedEvent for order id: {}", orderId );
-        PaymentResponseAvroModel paymentResponseAvroModel = paymentMessagingDataMapper.paymentFailedEventToPaymentResponseAvroModel(domainEvent);
-        // Kafka 메시지 전송
-        CompletableFuture<SendResult<String, PaymentResponseAvroModel>> future =
-                kafkaProducer.send(
-                        paymentServiceConfigData.getPaymentRequestTopicName(),
-                        orderId,
-                        paymentResponseAvroModel
-                );
-
-        // 전송 결과 처리
-        future.whenComplete((result, exception) ->
-                kafkaMessageHelper.getKafkaCallBack(result ,paymentServiceConfigData.getPaymentRequestTopicName() ,exception, orderId, ""));
-
-        log.info("PaymentResponseAvroModel sent to kafka for order id: {} and saga id: {}", paymentResponseAvroModel.getOrderId(), "");
-    }
-}
+//package com.food.ordering.system.payment.service.messaging.publisher.kafka;
+//
+//import com.food.ordering.system.domain.config.PaymentServiceConfigData;
+//import com.food.ordering.system.domain.ports.output.message.publisher.PaymentCancelledMessagePublisher;
+//import com.food.ordering.system.domain.ports.output.message.publisher.PaymentFailedMessagePublisher;
+//import com.food.ordering.system.kafka.order.avro.model.PaymentResponseAvroModel;
+//import com.food.ordering.system.kafka.producer.KafkaMessageHelper;
+//import com.food.ordering.system.kafka.producer.service.KafkaProducer;
+//import com.food.ordering.system.payment.service.domain.event.PaymentCancelledEvent;
+//import com.food.ordering.system.payment.service.domain.event.PaymentEvent;
+//import com.food.ordering.system.payment.service.domain.event.PaymentFailedEvent;
+//import com.food.ordering.system.payment.service.messaging.mapper.PaymentMessagingDataMapper;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.kafka.support.SendResult;
+//import org.springframework.stereotype.Component;
+//
+//import java.util.concurrent.CompletableFuture;
+//
+//@Slf4j
+//@Component
+//public class PaymentFailedKafkaMessagePublisher implements PaymentFailedMessagePublisher {
+//
+//    private final PaymentMessagingDataMapper paymentMessagingDataMapper;
+//    private final KafkaProducer<String, PaymentResponseAvroModel> kafkaProducer;
+//    private final PaymentServiceConfigData paymentServiceConfigData;
+//    private final KafkaMessageHelper kafkaMessageHelper;
+//
+//    public PaymentFailedKafkaMessagePublisher(PaymentMessagingDataMapper paymentMessagingDataMapper, KafkaProducer<String, PaymentResponseAvroModel> kafkaProducer, PaymentServiceConfigData paymentServiceConfigData, KafkaMessageHelper kafkaMessageHelper) {
+//        this.paymentMessagingDataMapper = paymentMessagingDataMapper;
+//        this.kafkaProducer = kafkaProducer;
+//        this.paymentServiceConfigData = paymentServiceConfigData;
+//        this.kafkaMessageHelper = kafkaMessageHelper;
+//    }
+//
+//
+//    @Override
+//    public void publish(PaymentFailedEvent domainEvent) {
+//
+//        String orderId = domainEvent.getPayment().getOrderId().getValue().toString();
+//        log.info("Received PaymentFailedEvent for order id: {}", orderId );
+//        PaymentResponseAvroModel paymentResponseAvroModel = paymentMessagingDataMapper.paymentFailedEventToPaymentResponseAvroModel(domainEvent);
+//        // Kafka 메시지 전송
+//        CompletableFuture<SendResult<String, PaymentResponseAvroModel>> future =
+//                kafkaProducer.send(
+//                        paymentServiceConfigData.getPaymentRequestTopicName(),
+//                        orderId,
+//                        paymentResponseAvroModel
+//                );
+//
+//        // 전송 결과 처리
+//        future.whenComplete((result, exception) ->
+//                kafkaMessageHelper.getKafkaCallBack(result ,paymentServiceConfigData.getPaymentRequestTopicName() ,exception, orderId, ""));
+//
+//        log.info("PaymentResponseAvroModel sent to kafka for order id: {} and saga id: {}", paymentResponseAvroModel.getOrderId(), "");
+//    }
+//}
